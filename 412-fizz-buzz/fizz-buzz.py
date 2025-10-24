eb764b2611315a480921e0cb971087334dd45acb
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-10-24 16:20:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (6.00%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        for i in range(1, n + 1):
+            ans.append(
+                "FizzBuzz" if i % 15 == 0 else
+                "Buzz" if i % 5 == 0 else
+                "Fizz" if i % 3 == 0 else
+                str(i)
+            )
+        return ans
+        
