37b3cce8e033172ad1ede00c12d8041ca6b9271b
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-11-14 11:42:23 +0530

    Time: 1 ms (70.25%) | Memory: 12.4 MB (92.77%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -5,12 +5,7 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-
         for num in nums:
-            temp = 0
-            while num!=0:
-                temp += 1
-                num /= 10
-            if temp%2==0:
+            if len(str(num))%2==0:
                 count += 1
         return count
